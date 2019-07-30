b3578d0a1a13bff3faef4b1b9f580dc13629475a
IDKiro <609568960@qq.com>
2019-07-30 14:16:13 +0800

    dataloader

---
+++ loader.py
@@ -0,0 +1,73 @@
+import os
+import random
+import torch
+import numpy as np
+import glob
+import PIL.Image as Image
+from torch.utils.data import Dataset
+
+from utils import read_img, hwc_to_chw
+
+
+def get_patch(origin_img, noise_img, patch_size):
+	H = origin_img.shape[0]
+	W = origin_img.shape[1]
+
+	ps_temp = min(H, W, patch_size + 1) - 1
+
+	xx = np.random.randint(0, W-ps_temp)
+	yy = np.random.randint(0, H-ps_temp)
+	
+	patch_origin_img = origin_img[yy:yy+ps_temp, xx:xx+ps_temp, :]
+	patch_noise_img = noise_img[yy:yy+ps_temp, xx:xx+ps_temp, :]
+
+	if np.random.randint(2, size=1)[0] == 1:
+		patch_origin_img = np.flip(patch_origin_img, axis=1).copy()
+		patch_noise_img = np.flip(patch_noise_img, axis=1).copy()
+	if np.random.randint(2, size=1)[0] == 1: 
+		patch_origin_img = np.flip(patch_origin_img, axis=0).copy()
+		patch_noise_img = np.flip(patch_noise_img, axis=0).copy()
+	if np.random.randint(2, size=1)[0] == 1:
+		patch_origin_img = np.transpose(patch_origin_img, (1, 0, 2)).copy()
+		patch_noise_img = np.transpose(patch_noise_img, (1, 0, 2)).copy()
+	
+	return patch_origin_img, patch_noise_img
+
+
+class loadedDataset(Dataset):
+	def __init__(self, root_dir, patch_size=128):
+		self.root_dir = root_dir
+		self.batches = sorted(os.listdir(self.root_dir))
+		self.patch_size = patch_size
+		self.origin_imgs = [None] * len(self.batches)
+		self.noise_imgs = [None] * len(self.batches)
+
+		for i in range(len(self.batches)):
+			self.origin_imgs[i] = []
+			self.noise_imgs[i] = []
+
+	def __len__(self):
+		l = len(self.batches)
+		return l
+
+	def __getitem__(self, idx):
+		batch_path = os.path.join(self.root_dir, self.batches[idx])
+
+		if not len(self.origin_imgs[idx]):
+			origin_fns = glob.glob(batch_path + '/*Reference.bmp')
+			noise_fns = glob.glob(batch_path + '/*Noisy.bmp')
+
+			self.origin_imgs[idx] = read_img(origin_fns[0])
+
+			for noise_fn in noise_fns:
+				self.noise_imgs[idx].append(read_img(noise_fn))
+				
+		origin_img = self.origin_imgs[idx]
+		noise_img = self.noise_imgs[idx][np.random.randint(len(self.noise_imgs[idx]))]
+
+		patch_origin_img, patch_noise_img = get_patch(origin_img, noise_img, patch_size=self.patch_size)
+
+		patch_origin_img_chw = hwc_to_chw(patch_origin_img)
+		patch_noise_img_chw = hwc_to_chw(patch_noise_img)
+
+		return patch_origin_img_chw, patch_noise_img_chw
--- train.py
+++ train.py
@@ -12,6 +12,7 @@ import torch.nn.functional as F
 
 from utils import *
 from model import *
+from loader import loadedDataset
 
 
 parser = argparse.ArgumentParser(description = 'Train')
@@ -36,34 +37,11 @@ def adjust_learning_rate(optimizer, epoch, lr_update_freq):
 	return optimizer
 
 
-def data_augment(temp_origin_img, temp_noise_img):
-    if np.random.randint(2, size=1)[0] == 1:
-        temp_origin_img = np.flip(temp_origin_img, axis=1)
-        temp_noise_img = np.flip(temp_noise_img, axis=1)
-    if np.random.randint(2, size=1)[0] == 1: 
-        temp_origin_img = np.flip(temp_origin_img, axis=0)
-        temp_noise_img = np.flip(temp_noise_img, axis=0)
-    if np.random.randint(2, size=1)[0] == 1:
-        temp_origin_img = np.transpose(temp_origin_img, (1, 0, 2))
-        temp_noise_img = np.transpose(temp_noise_img, (1, 0, 2))
-    
-    return temp_origin_img, temp_noise_img
-
-
 ps = args.ps
 save_freq = 100
 lr_update_freq = args.freq
 
 input_dir = './dataset/train/'
-train_fns = glob.glob(input_dir + 'Batch_*')
-
-origin_imgs = [None] * len(train_fns)
-noise_imgs = [None] * len(train_fns)
-
-for i in range(len(train_fns)):
-    origin_imgs[i] = []
-    noise_imgs[i] = []
-
 model = importlib.import_module('.' + args.model, package='model').Network()
 
 checkpoint_dir = os.path.join('./checkpoint/', args.model)
@@ -93,93 +71,59 @@ else:
     criterion = nn.L1Loss()
 criterion = criterion.cuda()
 
+train_dataset = loadedDataset(input_dir)
+train_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=1, shuffle=True, pin_memory=True)
+
 for epoch in range(cur_epoch, args.epochs + 1):
-    cnt=0
     losses = AverageMeter()
     optimizer = adjust_learning_rate(optimizer, epoch, lr_update_freq)
     model.train()
 
-    for ind in np.random.permutation(len(train_fns)):
-        train_fn = train_fns[ind]
-
-        if not len(origin_imgs[ind]):
-            train_origin_fns = glob.glob(train_fn + '/*Reference.bmp')
-            train_noise_fns = glob.glob(train_fn + '/*Noisy.bmp')
-
-            origin_imgs[ind] = read_img(train_origin_fns[0])
-
-            for train_noise_fn in train_noise_fns:
-                noise_img = read_img(train_noise_fn)
-                noise_imgs[ind].append(noise_img)
-
+    st = time.time()
+    for ind, (noise_img, origin_img) in enumerate(train_loader):
         st = time.time()
-        for nind in np.random.permutation(len(noise_imgs[ind])):
-            H = origin_imgs[ind].shape[0]
-            W = origin_imgs[ind].shape[1]
 
-            ps_temp = min(H, W, ps + 1) - 1
+        input_var = torch.autograd.Variable(noise_img).cuda()
+        target_var = torch.autograd.Variable(origin_img).cuda()
 
-            xx = np.random.randint(0, W-ps_temp)
-            yy = np.random.randint(0, H-ps_temp)
-            
-            temp_origin_img = origin_imgs[ind][yy:yy+ps_temp, xx:xx+ps_temp, :]
-            temp_noise_img = noise_imgs[ind][nind][yy:yy+ps_temp, xx:xx+ps_temp, :]
-            temp_origin_img, temp_noise_img = data_augment(temp_origin_img, temp_noise_img)
-            
-            temp_noise_img_chw = hwc_to_chw(temp_noise_img)
-            temp_origin_img_chw = hwc_to_chw(temp_origin_img)
+        # TODO: output
+        if args.model == 'cbdnet':
+            noise_level = noise_img - origin_img
+            noise_level_var = torch.autograd.Variable(noise_level).cuda()
+            noise_level_est, output = model(input_var)
+            loss = criterion(output, target_var, noise_level_est, noise_level_var)
+        else:
+            output = model(input_var)
+            loss = criterion(output, target_var)
 
-            cnt += 1
-            st = time.time()
+        losses.update(loss.item())
 
-            input_var = torch.autograd.Variable(
-                torch.from_numpy(temp_noise_img_chw.copy()).type(torch.FloatTensor).unsqueeze(0)
-                )
-            target_var = torch.autograd.Variable(
-                torch.from_numpy(temp_origin_img_chw.copy()).type(torch.FloatTensor).unsqueeze(0)
-                )
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-            input_var, target_var = input_var.cuda(), target_var.cuda()
+        print('[{0}][{1}]\t'
+            'lr: {lr:.5f}\t'
+            'Loss: {loss.val:.4f} ({loss.avg:.4f})\t'
+            'Time: {time:.3f}'.format(
+            epoch, ind,
+            lr=optimizer.param_groups[-1]['lr'],
+            loss=losses,
+            time=time.time()-st))
 
-            # TODO: output
-            if args.model == 'cbdnet':
-                noise_level = temp_noise_img - temp_origin_img
-                noise_level_chw = hwc_to_chw(noise_level)
-                noise_level_var = torch.autograd.Variable(
-                    torch.from_numpy(noise_level_chw.copy()).type(torch.FloatTensor).unsqueeze(0)
-                    )
-                noise_level_var = noise_level_var.cuda()
-                noise_level_est, output = model(input_var)
-                loss = criterion(output, target_var, noise_level_est, noise_level_var)
-            else:
-                output = model(input_var)
-                loss = criterion(output, target_var)
+        # if epoch % save_freq == 0:
+        #     if not os.path.isdir(os.path.join(result_dir, '%04d'%epoch)):
+        #         os.makedirs(os.path.join(result_dir, '%04d'%epoch))
 
-            losses.update(loss.item())
+        #     output_np = output.squeeze().cpu().detach().numpy()
+        #     output_np = chw_to_hwc(np.clip(output_np, 0, 1))
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        #     print(output_np.shape)
 
-            print('[{0}][{1}]\t'
-                'lr: {lr:.5f}\t'
-                'Loss: {loss.val:.4f} ({loss.avg:.4f})\t'
-                'Time: {time:.3f}'.format(
-                epoch, cnt,
-                lr=optimizer.param_groups[-1]['lr'],
-                loss=losses,
-                time=time.time()-st))
+        #     temp = np.concatenate((origin_img, noise_img, output_np), axis=1)
+        #     scipy.misc.toimage(temp*255, high=255, low=0, cmin=0, cmax=255).save(os.path.join(result_dir, '%04d/train_%d_%d.jpg'%(epoch, ind, nind)))
 
-            if epoch % save_freq == 0:
-                if not os.path.isdir(os.path.join(result_dir, '%04d'%epoch)):
-                    os.makedirs(os.path.join(result_dir, '%04d'%epoch))
-
-                output_np = output.squeeze().cpu().detach().numpy()
-                output_np = chw_to_hwc(np.clip(output_np, 0, 1))
-
-                temp = np.concatenate((temp_origin_img, temp_noise_img, output_np), axis=1)
-                scipy.misc.toimage(temp*255, high=255, low=0, cmin=0, cmax=255).save(os.path.join(result_dir, '%04d/train_%d_%d.jpg'%(epoch, ind, nind)))
-    
     save_checkpoint({
         'epoch': epoch + 1,
         'state_dict': model.state_dict(),
